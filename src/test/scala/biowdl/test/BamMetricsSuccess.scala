35689af54b73f5801d1e33b34ea510a89365eac7
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-09-07 09:26:58 +0200

    complete tests

--- src/test/scala/biowdl/test/BamMetricsSuccess.scala
+++ src/test/scala/biowdl/test/BamMetricsSuccess.scala
@@ -43,6 +43,9 @@ trait BamMetricsSuccess extends BamMetrics with PipelineSuccess {
   addMustHaveFile(prefix + ".quality_distribution_metrics")
   addMustHaveFile(prefix + ".quality_distribution.pdf")
   addMustHaveFile(prefix + ".quality_yield_metrics")
+  addMustHaveFile(prefix + ".bamstats")
+  addMustHaveFile(prefix + "_stats/bamstats.json")
+  addMustHaveFile(prefix + "_stats/bamstats.summary.json")
 
   addConditionalFile(rna, prefix + ".RNA_Metrics.pdf")
   addConditionalFile(rna, prefix + ".RNA_Metrics")
